233f15dab114409947fec8d3dc6b9c8190db4f94
dvoljatik <o.dvoljatik@cft.ru>
2020-09-06 01:27:56 +0700

    [screen-login] Добавил splash-screen

--- screen-login/src/main/java/ru/endroad/samples/login/application/SingleActivity.kt
+++ screen-login/src/main/java/ru/endroad/samples/login/application/SingleActivity.kt
@@ -1,14 +1,16 @@
 package ru.endroad.samples.login.application
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import ru.endroad.samples.login.R
 
 class SingleActivity : AppCompatActivity() {
 
-	override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-		super.onCreate(savedInstanceState, persistentState)
+	private val applicationThemeId = R.style.AppTheme
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		setTheme(applicationThemeId)
+		super.onCreate(savedInstanceState)
 		setContentView(R.layout.hub_activity)
 
 	}
