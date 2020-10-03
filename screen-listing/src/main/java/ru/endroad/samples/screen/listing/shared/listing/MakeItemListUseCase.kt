16ace598228b1322aeda2499d1943c028c86c98f
dvoljatik <o.dvoljatik@cft.ru>
2020-10-03 23:37:32 +0700

    [screen-listing] Добавил показ секций в списке

--- screen-listing/src/main/java/ru/endroad/samples/screen/listing/shared/listing/MakeItemListUseCase.kt
+++ screen-listing/src/main/java/ru/endroad/samples/screen/listing/shared/listing/MakeItemListUseCase.kt
@@ -7,6 +7,10 @@ import ru.endroad.samples.screen.listing.shared.series.Series
 
 class MakeItemListUseCase {
 
+	//TODO правильнее присылать ресурсы для имен секций в параметрах. Однако в sample-приложении решил этим пренебречь
+	private val sectionItemSeries = SectionItem("Сериалы")
+	private val sectionItemMovies = SectionItem("Фильмы")
+
 	operator fun invoke(
 		movies: List<Movie>?,
 		series: List<Series>?,
@@ -14,7 +18,11 @@ class MakeItemListUseCase {
 	): List<ListingItem> =
 		mutableListOf<ListingItem>().apply {
 			promo?.let(::PromoItem)?.let(::add)
+
+			sectionItemSeries.let(::add)
 			series?.map(::SeriesItem)?.let(::addAll)
+
+			sectionItemMovies.let(::add)
 			movies?.map(::MovieItem)?.let(::addAll)
 		}
 }
--- screen-listing/src/main/java/ru/endroad/samples/screen/listing/view/adapter/SectionViewHolder.kt
+++ screen-listing/src/main/java/ru/endroad/samples/screen/listing/view/adapter/SectionViewHolder.kt
@@ -1,12 +1,13 @@
 package ru.endroad.samples.screen.listing.view.adapter
 
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.item_section.view.*
 import ru.endroad.samples.screen.listing.R
 import ru.endroad.samples.screen.listing.shared.listing.ListingItem
 
 class SectionViewHolder(parent: ViewGroup) : ListingViewHolder(parent.inflate(R.layout.item_section)) {
 
 	fun bind(item: ListingItem.SectionItem) {
-
+		itemView.section_title.text = item.title
 	}
 }
