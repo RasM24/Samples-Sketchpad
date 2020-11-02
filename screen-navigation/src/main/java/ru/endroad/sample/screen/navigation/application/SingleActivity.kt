58944ef608e9837922aa12f8aaf600ed80b6e331
dvoljatik <o.dvoljatik@cft.ru>
2020-11-03 01:07:28 +0700

    [screen-navigation] Добавил модуль для демонстрации работы навигации

---
+++ screen-navigation/src/main/java/ru/endroad/sample/screen/navigation/application/SingleActivity.kt
@@ -0,0 +1,10 @@
+package ru.endroad.sample.screen.navigation.application
+
+import ru.endroad.libraries.camp.activity.CampActivity
+import ru.endroad.sample.screen.navigation.R
+
+class SingleActivity : CampActivity() {
+
+	override val layout = R.layout.root_activity
+	override val theme = R.style.ThemeSketchpad
+}
