11a9f9245b13df482d4ddd5cf6c4740e84ce960f
dvoljatik <o.dvoljatik@cft.ru>
2020-12-08 02:58:44 +0700

    [screen-coroutine] Добавил активити для показа примеров

---
+++ screen-coroutines/src/main/java/sample/endroad/coroutines/coroutine/CoroutineActivity.kt
@@ -0,0 +1,13 @@
+package sample.endroad.coroutines.coroutine
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import sample.endroad.coroutines.R
+
+class CoroutineActivity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.coroutine_activity)
+	}
+}
---
+++ screen-coroutines/src/main/java/sample/endroad/coroutines/flow/FlowActivity.kt
@@ -0,0 +1,13 @@
+package sample.endroad.coroutines.flow
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import sample.endroad.coroutines.R
+
+class FlowActivity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.flow_activity)
+	}
+}
