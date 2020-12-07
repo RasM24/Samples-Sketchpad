297b2a71c620508c799f0f6d03733f19624d6773
dvoljatik <o.dvoljatik@cft.ru>
2020-12-08 03:18:41 +0700

    [screen-coroutine] Добавил примеры с shared/state flow

--- screen-coroutines/src/main/java/sample/endroad/coroutines/flow/FlowActivity.kt
+++ screen-coroutines/src/main/java/sample/endroad/coroutines/flow/FlowActivity.kt
@@ -2,8 +2,13 @@ package sample.endroad.coroutines.flow
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.android.synthetic.main.coroutine_activity.view.*
+import kotlinx.android.synthetic.main.flow_activity.*
+import kotlinx.coroutines.flow.collect
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import sample.endroad.coroutines.R
+import sample.endroad.coroutines.application.appendText
 
 class FlowActivity : AppCompatActivity() {
 
@@ -12,5 +17,19 @@ class FlowActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.flow_activity)
+
+		setupViewComponent()
+		subscribeFlows()
+	}
+
+	private fun setupViewComponent() {
+		start_flow_shared_button.setOnClickListener { flowViewModel.exampleSharedFlow() }
+		start_flow_state_button.setOnClickListener { flowViewModel.exampleStateFlow() }
+		button_clear_log.setOnClickListener { log_view.text = null }
+	}
+
+	private fun subscribeFlows() {
+		lifecycleScope.launchWhenResumed { flowViewModel.sharedFlow.collect { log_view.log_view.appendText(it) } }
+		lifecycleScope.launchWhenResumed { flowViewModel.stateFlow.collect { start_flow_state_button.text = it } }
 	}
 }
--- screen-coroutines/src/main/java/sample/endroad/coroutines/flow/FlowViewModel.kt
+++ screen-coroutines/src/main/java/sample/endroad/coroutines/flow/FlowViewModel.kt
@@ -1,9 +1,38 @@
 package sample.endroad.coroutines.flow
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import sample.endroad.coroutines.application.MessageSender
 import sample.endroad.coroutines.application.MessageSenderImpl
+import kotlin.coroutines.CoroutineContext
 
 class FlowViewModel : ViewModel(), MessageSender by MessageSenderImpl() {
+
+	val sharedFlow = MutableSharedFlow<String>(extraBufferCapacity = 10)
+	val stateFlow = MutableStateFlow("Initialized State")
+
+	fun exampleSharedFlow() {
+		viewModelScope.launch {
+			sharedFlow.emit("first element")
+			delay(2000)
+			sharedFlow.emit("second element")
+			delay(1000)
+			sharedFlow.emit("third element")
+		}
+	}
+
+	fun exampleStateFlow() {
+		viewModelScope.launch {
+			stateFlow.tryEmit("Loading State")
+			delay(4000)
+			stateFlow.tryEmit("Playing State")
+			delay(2000)
+			stateFlow.tryEmit("Pausa State")
+		}
+	}
 
 }
