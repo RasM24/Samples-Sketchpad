5c1475a085af52244967892afecaa6dd649a3144
dvoljatik <o.dvoljatik@cft.ru>
2020-12-11 00:34:24 +0700

    [screen-coroutine] Подготовил активити под примеры корутин

--- screen-coroutines/src/main/java/sample/endroad/coroutines/reality/RealityActivity.kt
+++ screen-coroutines/src/main/java/sample/endroad/coroutines/reality/RealityActivity.kt
@@ -2,6 +2,9 @@ package sample.endroad.coroutines.reality
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.android.synthetic.main.reality_activity.*
+import kotlinx.coroutines.flow.collect
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import sample.endroad.coroutines.R
 
@@ -12,5 +15,15 @@ class RealityActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.reality_activity)
+
+		setupViewComponent()
+	}
+
+	private fun setupViewComponent() {
+		fetch_repositories_button.setOnClickListener { viewModel.fetchRepositories() }
+		send_private_request.setOnClickListener { viewModel.sendPrivateRequest() }
+
+		button_clear_log.setOnClickListener { log_view.text = null }
+		lifecycleScope.launchWhenResumed { viewModel.messages.collect { log_view.text = it } }
 	}
 }
--- screen-coroutines/src/main/java/sample/endroad/coroutines/reality/RealityViewModel.kt
+++ screen-coroutines/src/main/java/sample/endroad/coroutines/reality/RealityViewModel.kt
@@ -3,7 +3,17 @@ package sample.endroad.coroutines.reality
 import androidx.lifecycle.ViewModel
 import sample.endroad.coroutines.application.MessageSender
 import sample.endroad.coroutines.application.MessageSenderImpl
+import sample.endroad.coroutines.reality.data.GithubDataSource
 
-class RealityViewModel : ViewModel(), MessageSender by MessageSenderImpl() {
+class RealityViewModel(
+	private val dataSource: GithubDataSource
+) : ViewModel(), MessageSender by MessageSenderImpl() {
 
+	fun fetchRepositories() {
+
+	}
+
+	fun sendPrivateRequest() {
+
+	}
 }
