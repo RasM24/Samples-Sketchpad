e8a8f7b57289541cf912f9dcad214ba7839cc986
o.dvolyatik <o.dvolyatik@gmail.com>
2021-03-28 18:30:22 +0700

    [compose] Добавил новый фрагмент для использования JetpackCompose

---
+++ screen-login/src/main/java/ru/endroad/samples/login/view/LoginComposeFragment.kt
@@ -0,0 +1,38 @@
+package ru.endroad.samples.login.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import kotlinx.coroutines.flow.StateFlow
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class LoginComposeFragment : Fragment() {
+
+	private val viewModel: LoginViewModel by viewModel()
+
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
+		ComposeView(requireContext()).apply {
+			setContent {
+				App(viewModel.state)
+			}
+		}
+
+	@Composable
+	fun App(stateFlow: StateFlow<LoginScreenState>) {
+		MaterialTheme {
+			Scaffold {
+				when (val state = stateFlow.collectAsState().value) {
+					is LoginScreenState.Initialized -> RenderStubView()
+					is LoginScreenState.VerifyCode  -> RenderStubView()
+				}
+			}
+		}
+	}
+}
